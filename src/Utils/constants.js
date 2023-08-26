bc3ff231081fd894844f421dd1417e301a8e9943
AdagiosByte <james.keir4@gmail.com>
2023-08-26 19:48:20 -0400

    added all page routes and added functional footer.

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -9,3 +9,25 @@ export const PageName = [
     {name: 'Blog', link: '/Blog'},
     {name: 'Contact Us', link: '/Contact'}
 ]
+
+export const Company = [
+    {name: 'About Us', link: '/Company'},
+    {name: 'Contact Us', link: '/Contact'},
+    {name: 'Careers', link: '/Careers'}
+]
+
+//Footer Items
+export const Products = [
+    {name: 'SAFSIMS', link: 'https://safsims.com'},
+    {name: 'SAFAPPLY', link: 'https://www.safapply.com'},
+    {name: 'SRMS', link: 'https://srms.ng'}
+]
+
+export const Resources = [
+    {name: 'Contact', link: '/Contact'}
+]
+
+export const Legal = [
+    {name: 'Privacy', link: '/Privacy'},
+    {name: 'Terms-of-Service', link: '/Terms-of-Service'}
+]
